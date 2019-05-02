57c836a42e4c86c2a4b695419e8ae72801e8738e
okarin <les.thibautl.book@gmail.com>
2019-05-03 00:07:29 +0900

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@4)50uivn=y!lbz=7i&#jvwkrc%!@tzqsf)0at3717(jmxy_yv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
